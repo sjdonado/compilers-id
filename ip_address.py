2597727b6ddce0a3e5c895a0992d894f207d47fd
Brian Ramirez <brians@uninorte.edu.co>
2019-11-01 22:42:00 -0500

    feat: ip_address   - Implement validate method   - Implement test cases

---
+++ ip_address.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/python3
+import re
+
+def validate(args):
+  line = "".join(args)
+  octet = r'(([0-9])|([1-9][0-9])|(1([0-9]{2}))|(2([0-4][0-9]|5[0-5])))'
+  return re.match(fr'{octet}\.{octet}\.{octet}\.{octet}$', line)
+
+def main():
+  date_input = input('Ingresar dirección ip: ')
+  print("{}".format('Dirección ip valida' if validate(date_input) else 'Dirección ip invalida'))
+
+def test():
+  assert validate(['4.20.109.3']) is not None, 'Should not be None'
+  assert validate(['122.122.255.0']) is not None, 'Should not be None'
+  assert validate(['4.1.10.41']) is not None, 'Should not be None'
+  assert validate(['124.20.255.255']) is not None, 'Should not be None'
+
+  assert validate(['267.0.109.3']) is None, 'Should be None'
+  assert validate(['1.01.255.0']) is None, 'Should be None'
+  assert validate(['0.0.289.299']) is None, 'Should be None'
+  assert validate(['198.0.299.299']) is None, 'Should be None'
+
+  print('Tests passed successfully')
+
+if __name__ == '__main__':
+  test()
+  main()
